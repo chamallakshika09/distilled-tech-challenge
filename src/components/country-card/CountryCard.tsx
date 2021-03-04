e0932bc4bdd1bce32328f1c43311ba84f3a731b7
Chamal Perera <chamallakshika09@gmail.com>
2021-03-04 22:23:06 +0530

    feat(CountryTitle): implement country title component

--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Country } from '../../utils';
 
 import { Card } from '../card';
+import { CountryTitle } from '../country-title';
 import { Image } from '../flag-image';
 
 interface CountryCardProps {
@@ -12,6 +13,7 @@ export const CountryCard: FC<CountryCardProps> = ({ country }) => {
   return (
     <Card>
       <Image src={country.flag} alt={`${country.name} flag`} height="70px" />
+      <CountryTitle>{country.name}</CountryTitle>
     </Card>
   );
 };
---
+++ src/components/country-title/styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const CountryTitle = styled.h3`
+  color: grey;
+  font-size: 16px;
+  text-align: center;
+`;

2baa75ab5c832fc733d80bf4279194c421fbb1e3
Chamal Perera <chamallakshika09@gmail.com>
2021-03-04 23:07:02 +0530

    refactor(Card): refactor card style

--- src/components/card/styles.tsx
+++ src/components/card/styles.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  min-width: 300px;
-  min-height: 300px;
+  min-width: 200px;
+  min-height: 250px;
   box-shadow: 0 4px 8px 0 gray;
   border: 2px solid black;
   flex: 1;

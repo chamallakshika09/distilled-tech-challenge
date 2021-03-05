ab7e4abeaa32462854acff560e390801549d9fbc
Chamal Perera <chamallakshika09@gmail.com>
2021-03-05 16:52:00 +0530

    styles(Card): change styles of Card and CardList components

--- src/components/card-list/styles.tsx
+++ src/components/card-list/styles.tsx
@@ -4,7 +4,6 @@ export const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex: 1;
-  margin: 20px;
   justify-content: center;
   align-items: center;
 `;
--- src/components/card/styles.tsx
+++ src/components/card/styles.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Card = styled.div`
   min-width: 200px;
   min-height: 250px;
+  max-width: 200px;
   box-shadow: 0 4px 8px 0 gray;
   border: 2px solid black;
-  flex: 1;
   margin: 10px;
 `;

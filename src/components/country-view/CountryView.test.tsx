9fc0b8e1905c81c0e5fc7d2fd3b4ab8d8c900d6f
Chamal Perera <chamallakshika09@gmail.com>
2021-03-05 18:59:35 +0530

    test(CountryView): implement tests for CountryView component

--- src/components/country-view/CountryView.test.tsx
+++ src/components/country-view/CountryView.test.tsx
@@ -1,24 +1,24 @@
 import { render, screen } from '@testing-library/react';
-import { COUNTRIES, formatNumbers } from '../../utils';
-import { CountryCard } from './CountryCard';
+import { COUNTRIES, formatNumbers, getFormattedList } from '../../utils';
+import { CountryView } from './CountryView';
 
 describe('CountryCard', () => {
   it('should render a single country card', () => {
     const country = COUNTRIES[0];
-    render(<CountryCard country={country} />);
+    render(<CountryView countries={COUNTRIES} countryCode={country.alpha3Code} />);
 
     const flagElement = screen.getByRole('img');
-    const titleElement = screen.getByRole('heading');
+    const titleElement = screen.getByRole('heading', { level: 3 });
     const capitalElement = screen.getByText(`Capital: ${country.capital}`);
     const populationElement = screen.getByText(`Population: ${formatNumbers(country.population)}`);
-    const regionElement = screen.getByText(`Region: ${country.region}`);
-    const areaElement = screen.getByText(`Area: ${country.area ? formatNumbers(country.area) : '-'}`);
+    const currencyElement = screen.getByText(`Currency: ${getFormattedList(country.currencies)}`);
+    const languageElement = screen.getByText(`Languages: ${getFormattedList(country.languages)}`);
 
     expect(flagElement).toBeDefined();
     expect(titleElement).toHaveTextContent(country.name);
     expect(capitalElement).toBeDefined();
     expect(populationElement).toBeDefined();
-    expect(regionElement).toBeDefined();
-    expect(areaElement).toBeDefined();
+    expect(currencyElement).toBeDefined();
+    expect(languageElement).toBeDefined();
   });
 });

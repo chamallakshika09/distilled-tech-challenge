10c0751bfc99a9afa4b810a797fdd0ad9c0ab770
Chamal Perera <chamallakshika09@gmail.com>
2021-03-05 14:43:03 +0530

    feat(Button): implement Button component

--- src/pages/country-details/CountryDetails.tsx
+++ src/pages/country-details/CountryDetails.tsx
@@ -1,18 +1,26 @@
-import React, { FC } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { FC, useContext } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { AppTitle } from '../../components/app-title';
+import { Button } from '../../components/button';
 import { Container } from '../../components/container';
+import { CountryContext } from '../../context';
 
 const CountryDetails: FC = () => {
+  const { state } = useContext(CountryContext);
+  const { countries } = state;
   const params = useParams();
+  const history = useHistory();
 
   const { countryCode } = params as { countryCode: string };
 
-  console.log(countryCode);
+  const handleClick = () => {
+    history.push(`/`);
+  };
 
   return (
     <Container>
       <AppTitle>Countries App</AppTitle>
+      <Button onClick={handleClick}>Back</Button>
     </Container>
   );
 };

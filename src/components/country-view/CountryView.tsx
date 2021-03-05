59583726b087ae007526255f6ab5a7ddd4a72fb0
Chamal Perera <chamallakshika09@gmail.com>
2021-03-05 16:19:13 +0530

    feat(CountryView): add border countries to CountryView component

--- src/components/country-view/CountryView.tsx
+++ src/components/country-view/CountryView.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import { Country } from '../../utils';
+import { CardList } from '../card-list';
 import { CountryTitle } from '../country-title';
 import { DescriptionSection } from '../description-section';
 import { Image } from '../flag-image';
+import { BorderingCountriesSectionTitle } from './styles';
 
 interface CountryViewProps {
   countryCode: string;
@@ -11,12 +13,16 @@ interface CountryViewProps {
 
 export const CountryView: FC<CountryViewProps> = ({ countryCode, countries }) => {
   const country = countries.filter((country) => country.alpha3Code === countryCode)[0];
+  const { borders } = country;
+  const borderCountries = countries.filter((country) => borders.indexOf(country.alpha3Code) > -1);
 
   return (
     <React.Fragment>
       <Image src={country.flag} alt={`${country.name} flag`} height="200px" />
       <CountryTitle>{country.name}</CountryTitle>
       <DescriptionSection country={country} />
+      <BorderingCountriesSectionTitle>Bordering Countries</BorderingCountriesSectionTitle>
+      <CardList countries={borderCountries} />
     </React.Fragment>
   );
 };
---
+++ src/components/country-view/styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BorderingCountriesSectionTitle = styled.h4`
+  font-size: 14px;
+  text-align: left;
+  margin: 20px;
+`;
